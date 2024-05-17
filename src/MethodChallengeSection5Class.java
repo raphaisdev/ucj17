4f4b32626ce3b2adfd46770e845d2b61ab47240c
Raphael <raphael.galera@ftd.com.br>
2024-05-17 15:27:08 -0300

    section 5

---
+++ src/MethodChallengeSection5Class.java
@@ -0,0 +1,38 @@
+package src;
+
+public class MethodChallengeSection5Class {
+    public static void main(String[] args) {
+        int highScorePosition = calculateHighScorePosition(1500);
+        displayHighScorePosition("Player1", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(1000);
+        displayHighScorePosition("Player2", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(500);
+        displayHighScorePosition("Player3", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(100);
+        displayHighScorePosition("Player4", highScorePosition);
+
+        highScorePosition = calculateHighScorePosition(50);
+        displayHighScorePosition("Player5", highScorePosition);
+    }
+
+    public static void displayHighScorePosition( String playerName, int highScorePosition) {
+        System.out.println(playerName + " got score position: " + highScorePosition);
+    }
+
+    public static int calculateHighScorePosition(int playerScore) {
+        if (playerScore >= 1000) {
+            return 1;
+        }
+        if (playerScore >= 500 && playerScore < 1000) {
+            return 2;
+        }
+        if (playerScore >= 100 && playerScore < 500) {
+            return 3;
+        }
+        return 4;
+    }
+
+}
---
+++ src/StatementsClass.java
@@ -0,0 +1,81 @@
+package src;
+
+public class StatementsClass {
+    public static void main(String[] args) {
+        int myInt = 50;
+        myInt++;
+        myInt--;
+        System.out.println(myInt);
+
+        System.out.println("Test1");
+        System.out.println("Test2" + " teste3 ");
+        System.out.println("Test4" +
+                " teste5 " +
+                "teste6");
+
+        boolean gameOver = true;
+        int score = 5000;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        if (score == 5000) {
+            System.out.println("You win!");
+        } else if (score < 5000) {
+            System.out.println("You lose!");
+        } else {
+            System.out.println("Draw!");
+        }
+
+        int finalScore = score;
+
+        if (gameOver) {
+            finalScore += (levelCompleted * bonus);
+            System.out.println("Your final score is: " + finalScore);
+        }
+
+        boolean gameOver1 = true;
+        int score1 = 10000;
+        int levelCompleted1 = 8;
+        int bonus1 = 200;
+        int finalScore1 = score1;
+
+        if (gameOver1) {
+            finalScore1 += (levelCompleted1 * bonus1);
+            System.out.println("Your final score is: " + finalScore1);
+        }
+
+        score1 = 1000;
+        levelCompleted1 = 7;
+        bonus1 = 150;
+        finalScore1 = score1;
+
+        if (gameOver1) {
+            finalScore1 += (levelCompleted1 * bonus1);
+            System.out.println("Your final score is: " + finalScore1);
+        }
+
+        calculateScore(gameOver, score, levelCompleted, bonus);
+        calculateScore(true, 7200, 11, 350);
+
+        System.out.println("Your final score is: " + returnedCalculateScore(gameOver, score, levelCompleted, bonus));
+
+    }
+
+    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+        int finalScore = score;
+        if (gameOver) {
+            finalScore += (levelCompleted * bonus);
+            finalScore += (levelCompleted + bonus);
+            System.out.println("Your final score is: " + finalScore);
+        }
+    }
+
+    public static int returnedCalculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+        int finalScore = score;
+        if (gameOver) {
+            finalScore += (levelCompleted * bonus);
+            finalScore += (levelCompleted + bonus);
+        }
+        return finalScore;
+    }
+}
