d3c5f4e668ad391d3dfe4547f0c69bd79580b699
Raphael <raphael.galera@ftd.com.br>
2024-05-17 15:59:36 -0300

    section 5 exercise 4

---
+++ src/BarkingDogClass.java
@@ -0,0 +1,7 @@
+package src;
+
+public class BarkingDogClass {
+    public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
+        return (hourOfDay >= 0 && hourOfDay <= 23) && (barking && (hourOfDay < 8 || hourOfDay > 22));
+    }
+}
