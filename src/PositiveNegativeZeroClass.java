75b6a08798d997294512647964b7feaaa074fc94
Raphael <raphael.galera@ftd.com.br>
2024-05-17 15:27:22 -0300

    section 5 exercise 1

---
+++ src/PositiveNegativeZeroClass.java
@@ -0,0 +1,18 @@
+package src;
+
+public class PositiveNegativeZeroClass {
+    public static void checkNumber(int number) {
+        if(number < 0) {
+            System.out.println("negative");
+        } else if(number > 0) {
+            System.out.println("positive");
+        } else {
+            System.out.println("zero");
+        }
+    }
+
+    public static void ternaryCheckNumber(int number) {
+        String result = (number < 0) ? "negative" : (number > 0) ? "positive" : "zero";
+        System.out.println(result);
+    }
+}
