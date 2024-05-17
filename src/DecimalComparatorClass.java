fd01e20089efd67f4e8e3c3ad9c2a065ed57f761
Raphael <raphael.galera@ftd.com.br>
2024-05-17 16:42:59 -0300

    section 5 exercise 6

---
+++ src/DecimalComparatorClass.java
@@ -0,0 +1,9 @@
+package src;
+
+public class DecimalComparatorClass {
+    public static boolean areEqualByThreeDecimalPlaces(double first, double second) {
+        long firstRounded = (long) (first * 1000);
+        long secondRounded = (long) (second * 1000);
+        return firstRounded == secondRounded;
+    }
+}
