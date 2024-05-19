b148704f3345f1ee277cde31093460da76f6e938
Raphael <raphael.galera@ftd.com.br>
2024-05-19 15:37:37 -0300

    section 5 exercise 8

---
+++ src/TeenNumberCheckerClass.java
@@ -0,0 +1,11 @@
+package src;
+
+public class TeenNumberCheckerClass {
+    public static boolean hasTeen(int a, int b, int c) {
+        return (isTeen(a) || isTeen(b) || isTeen(c));
+    }
+
+    public static boolean isTeen(int a) {
+        return (a >= 13 && a <= 19);
+    }
+}
